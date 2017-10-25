6f23716925877bfd0d9f3472757fe20505ac5634
Andre <noromoron@gmail.com>
2017-10-26 01:16:59 +0200

    Add fill_rgb() function.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,21 @@
+use std::net::UdpSocket;
+
+
+fn fill_rgb(sock: &UdpSocket, address: &String, len: usize, r: u8, g: u8, b: u8) -> usize {
+    let mut bytes = vec![0 as u8; len*3];
+    for i in 0..len {
+        bytes[(i*3) + 0] = g;
+        bytes[(i*3) + 1] = r;
+        bytes[(i*3) + 2] = b;
+    }
+    sock.send_to(&bytes, &address).expect("error sending data")
+}
+
 
 fn main() {
-    println!("Hello, world!");
+    let socket = UdpSocket::bind("0.0.0.0:10400").expect(
+        "Could not connect");
+
+    let bytes_sent = fill_rgb(&socket, &"nomled:1234".to_string(), 20, 200, 100, 20);
+    println!("Total bytes sent: {:?}", bytes_sent);
 }
